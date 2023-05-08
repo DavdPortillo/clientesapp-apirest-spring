a7038ebd71be240c61e9e3a9a475ef5fb787d376
DavidPortillo <88davd@gmail.com>
2023-05-08 17:58:50 +0200

    backend-proyect

---
+++ apirest/apirest/src/main/java/com/api/entity/Client.java
@@ -0,0 +1,73 @@
+package com.api.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "client")
+public class Client implements Serializable{
+
+    
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getLastname() {
+	return lastname;
+    }
+
+    public void setLastname(String lastname) {
+	this.lastname = lastname;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public Date getCreateAt() {
+	return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+	this.createAt = createAt;
+    }
+    
+    private static final long serialVersionUID = 1L;
+
+}
